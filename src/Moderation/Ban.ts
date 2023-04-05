321d33e27f7f16bfe20bf4a3162f200dadd4d912
Ghex <69035887+NotGhex@users.noreply.github.com>
2023-04-05 14:01:58 +0800

    simple ban & kick

---
+++ src/Moderation/Ban.ts
@@ -0,0 +1,51 @@
+import { RecipleClient, SlashCommandBuilder } from 'reciple';
+import Utility from '../Internals/Utility.js';
+import BaseModule from '../BaseModule.js';
+import ms from 'ms';
+
+export class Ban extends BaseModule {
+    public async onStart(client: RecipleClient<boolean>): Promise<boolean> {
+        this.commands = [
+            new SlashCommandBuilder()
+                .setName('ban')
+                .setDescription('Ban a member')
+                .addUserOption(member => member
+                    .setName('target')
+                    .setDescription('User to ban')
+                    .setRequired(true)
+                )
+                .addStringOption(reason => reason
+                    .setName('reason')
+                    .setDescription('Reason for banning')
+                )
+                .addStringOption(deleteMessages => deleteMessages
+                    .setName('deleted-messages')
+                    .setDescription('How much of their recent messages history to delete')
+                    .setRequired(false)
+                )
+                .setExecute(async ({ interaction }) => {
+                    if (!interaction.inCachedGuild()) return;
+
+                    const targetUser = interaction.options.getUser('target', true);
+                    const reason = interaction.options.getString('reason');
+                    const deleteMessages = interaction.options.getString('delete-messages');
+                    const deleteMessagesMs = deleteMessages ? ms(deleteMessages) : undefined;
+
+                    await interaction.deferReply();
+
+                    const targetMember = await Utility.resolveFromCachedManager(targetUser.id, interaction.guild.members);
+                    if (!targetMember) await interaction.editReply('Member not found')
+                    if (!targetMember.bannable) await interaction.editReply('Unable to ban this user');
+
+                    await targetMember.ban({
+                        reason: reason || undefined,
+                        deleteMessageSeconds: deleteMessagesMs ? (deleteMessagesMs / 1000) : undefined
+                    });
+
+                    await interaction.editReply(`<:ban:1093051725142818876> **${targetUser.tag}** banned from the server`);
+                })
+        ];
+
+        return true;
+    }
+}
---
+++ src/Moderation/Kick.ts
@@ -0,0 +1,40 @@
+import { RecipleClient, SlashCommandBuilder } from 'reciple';
+import Utility from '../Internals/Utility.js';
+import BaseModule from '../BaseModule.js';
+
+export class Kick extends BaseModule {
+    public async onStart(client: RecipleClient<boolean>): Promise<boolean> {
+        this.commands = [
+            new SlashCommandBuilder()
+                .setName('kick')
+                .setDescription('Kick a member')
+                .addUserOption(member => member
+                    .setName('target')
+                    .setDescription('User to kick')
+                    .setRequired(true)
+                )
+                .addStringOption(reason => reason
+                    .setName('reason')
+                    .setDescription('Reason for kick')
+                )
+                .setExecute(async ({ interaction }) => {
+                    if (!interaction.inCachedGuild()) return;
+
+                    const targetUser = interaction.options.getUser('target', true);
+                    const reason = interaction.options.getString('reason');
+
+                    await interaction.deferReply();
+
+                    const targetMember = await Utility.resolveFromCachedManager(targetUser.id, interaction.guild.members);
+                    if (!targetMember) await interaction.editReply('Member not found')
+                    if (!targetMember.kickable) await interaction.editReply('Unable to kick this user');
+
+                    await targetMember.kick(reason || undefined);
+
+                    await interaction.editReply(`<:kick:1093052742840352778> **${targetUser.tag}** kicked from the server`);
+                })
+        ];
+
+        return true;
+    }
+}
